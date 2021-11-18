bb80bb15b1fd23acf273f227545b8e724f3cd5fc
haya <haya.Adblrhman.med.s@tuwaiq.edu.sa>
2021-11-18 13:25:31 +0300

    Initial Commit

---
+++ core data2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  core data2
+//
+//  Created by Haya on 13/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
